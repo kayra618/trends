c6d5d4ef65ab0e533b1ffa7f706e6096a9839f15
kayra618 <kayracengizz58@gmail.com>
2025-05-18 11:03:42 +0300

    Update main.py
    Signed-off-by: kayra618 <kayracengizz58@gmail.com>

--- main.py
+++ main.py
@@ -7,30 +7,31 @@ from datetime import date
 from google import genai
 from pydantic import BaseModel
 
-ara=st.text_input("Haber İçinde Arama Yap")
+ara = st.text_input("Haber İçinde Arama Yap")
 
-conn=sqlitecloud.connect('sqlitecloud://cwcgjb0ahz.g1.sqlite.cloud:8860/chinook.sqlite?apikey=DaG8uyqMPa9GdxoR7ObMoajHIdfUOrc7B0mF0IrU6Y0')
-c=conn.cursor()
+conn = sqlitecloud.connect('sqlitecloud://cwcgjb0ahz.g1.sqlite.cloud:8860/chinook.sqlite?apikey=DaG8uyqMPa9GdxoR7ObMoajHIdfUOrc7B0mF0IrU6Y0')
+c = conn.cursor()
 
-if len(ara)>1:
+if len(ara) > 1:
     c.execute(f"SELECT * FROM haberler WHERE baslik LIKE '%{ara}%' ORDER BY trend_id DESC LIMIT 99 ")
-
 else:
     c.execute("SELECT * FROM haberler ORDER BY trend_id DESC LIMIT 99")
-    
-haberler=c.fetchall()
 
-for i in range(0,len(haberler),3):
-    col1,col2,col3=st.columns(3)
+haberler = c.fetchall()
+
+for i in range(0, len(haberler), 3):
+    col1, col2, col3 = st.columns(3)
     with col1:
         st.image(haberler[i][3])
         st.write(haberler[i][1])
-        st.link_button("Habere Git",haberler[i][2])
-    with col2:
-        st.image(haberler[i+1][3])
-        st.write(haberler[i+1][1])
-        st.link_button("Habere Git",haberler[i+1][2])
-    with col3:
-        st.image(haberler[i+2][3])
-        st.write(haberler[i+2][1])
-        st.link_button("Habere Git",haberler[i+2][2])
+        st.link_button("Habere Git", haberler[i][2])
+    if i + 1 < len(haberler):
+        with col2:
+            st.image(haberler[i+1][3])
+            st.write(haberler[i+1][1])
+            st.link_button("Habere Git", haberler[i+1][2])
+    if i + 2 < len(haberler):
+        with col3:
+            st.image(haberler[i+2][3])
+            st.write(haberler[i+2][1])
+            st.link_button("Habere Git", haberler[i+2][2])
