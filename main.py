04de43e730bfeaf4fc81eca80c585000c4831e95
kayra618 <kayracengizz58@gmail.com>
2025-05-18 11:38:51 +0300

    Update main.py
    Signed-off-by: kayra618 <kayracengizz58@gmail.com>

--- main.py
+++ main.py
@@ -1,37 +1,40 @@
 import streamlit as st
-import xml.etree.ElementTree as et
 import sqlitecloud
-import sqlite3
-import requests
 from datetime import date
-from google import genai
 from pydantic import BaseModel
 from fonksiyonlar import trendgetir
 
-diller=["TR","DE","IT","KR","NL","DK"]
+# Kullanılan diller listesi
+diller = ["TR", "DE", "IT", "KR", "NL", "DK"]
 
-güncelle=st.sidebar.button("haberleri güncelle")
+# Güncelle butonu (sadece basınca çalışacak)
+güncelle = st.sidebar.button("Haberleri Güncelle")
 
+if güncelle:
+    for dil in diller:
+        trendgetir(dil)
 
-for dil in diller:
-    trendgetir(dil)
-    
-
+# Arama kutusu
 ara = st.text_input("Haber İçinde Arama Yap")
 
-conn = sqlitecloud.connect('sqlitecloud://cwcgjb0ahz.g1.sqlite.cloud:8860/chinook.sqlite?apikey=DaG8uyqMPa9GdxoR7ObMoajHIdfUOrc7B0mF0IrU6Y0')
+# Veritabanı bağlantısı
+conn = sqlitecloud.connect(
+    'sqlitecloud://cwcgjb0ahz.g1.sqlite.cloud:8860/chinook.sqlite?apikey=DaG8uyqMPa9GdxoR7ObMoajHIdfUOrc7B0mF0IrU6Y0'
+)
 c = conn.cursor()
 
+# Güvenli arama sorgusu
 if len(ara) > 1:
-    c.execute(f"SELECT * FROM haberler WHERE baslik LIKE '%{ara}%' ORDER BY trend_id DESC LIMIT 99 ")
+    c.execute("SELECT * FROM haberler WHERE baslik LIKE ? ORDER BY trend_id DESC LIMIT 99", ('%' + ara + '%',))
 else:
     c.execute("SELECT * FROM haberler ORDER BY trend_id DESC LIMIT 99")
 
 haberler = c.fetchall()
 
-if len(haberler)==0:
-    st.warning(f"  {ara} sorgusu ile ilgili herhangi bir haber bulunamadı")
+if len(haberler) == 0:
+    st.warning(f"'{ara}' sorgusu ile ilgili herhangi bir haber bulunamadı")
 
+# Haberleri üçlü gruplar halinde göster
 for i in range(0, len(haberler), 3):
     col1, col2, col3 = st.columns(3)
     with col1:
@@ -46,5 +49,5 @@ for i in range(0, len(haberler), 3):
     if i + 2 < len(haberler):
         with col3:
             st.image(haberler[i+2][3])
-            st.write(haberler[i+2][1])
-            st.link_button("Habere Git", haberler[i+2][2])
+            st.write(haberler*
+
