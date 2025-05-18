9a7235724577ae1f31f8014183c0d62c12754b4f
kayra618 <kayracengizz58@gmail.com>
2025-05-18 11:34:57 +0300

    Update main.py
    Signed-off-by: kayra618 <kayracengizz58@gmail.com>

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ import requests
 from datetime import date
 from google import genai
 from pydantic import BaseModel
+from fonksiyonlar import trendgetir
+
+diller=["TR","DE","IT","KR","NL","DK"]
+
+güncelle=st.sidebar.button("haberleri güncelle")
+
+for dil in diller:
+    trendgetir(dil)
+    
 
 ara = st.text_input("Haber İçinde Arama Yap")
 
